d82961ba39886e365d8793051088c490c4c655bd
Jack Dorland <jack@jackdor.land>
2023-05-28 18:00:16 -0400

    Broken w/o sidebar

--- site.hs
+++ site.hs
@@ -1,67 +1,95 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
 
+import Data.Monoid (mappend)
+import Hakyll
+    ( getResourceBody,
+      saveSnapshot,
+      loadAll,
+      loadAllSnapshots,
+      copyFileCompiler,
+      idRoute,
+      setExtension,
+      compile,
+      match,
+      route,
+      hakyll,
+      compressCssCompiler,
+      relativizeUrls,
+      pandocCompiler,
+      dateField,
+      defaultContext,
+      listField,
+      teaserField,
+      applyAsTemplate,
+      loadAndApplyTemplate,
+      templateBodyCompiler,
+      recentFirst,
+      Context )
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
+  match "assets/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "assets/*" $ do
-	route idRoute
-	compile copyFileCompiler
+  match "assets/images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "assets/images/*" $ do
-    	route idRoute
-	compile copyFileCompiler
+  match "assets/stylesheets/*" $ do
+    route idRoute
+    compile compressCssCompiler
+  
+  match "posts/*.md" $ do
+    route $ setExtension "html"
+    compile $ do
+      posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
+      let otherBlogPostsCtx = 
+           dateField "date" "%B %e, %y" `mappend`
+            listField "posts" defaultPostCtx (return posts) `mappend`
+            defaultContext
 
-    match "assets/stylesheets/*" $ do
-    	route idRoute
-	compile compressCssCompiler
+      pandocCompiler
+        >>= applyAsTemplate otherBlogPostsCtx
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html" defaultPostCtx
+        >>= relativizeUrls
 
-    match "posts/*.md" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-	    >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= relativizeUrls
-    	
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
-            let indexCtx =
-                    listField "posts" teaserCtx (return (take 1 posts)) `mappend`
-                    defaultContext
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
+      let indexCtx =
+            listField "posts" teaserCtx (return (take 1 posts))
+              `mappend` defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/index.html" indexCtx
-                >>= relativizeUrls
-    
-    match "blog.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let blogPostsCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/index.html" indexCtx
+        >>= relativizeUrls
 
-            getResourceBody
-                >>= applyAsTemplate blogPostsCtx
-                >>= loadAndApplyTemplate "templates/blog.html" blogPostsCtx
-                >>= relativizeUrls
+  match "blog.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let blogPostsCtx =
+            listField "posts" defaultPostCtx (return posts)
+              `mappend` defaultContext
 
+      getResourceBody
+        >>= applyAsTemplate blogPostsCtx
+        >>= loadAndApplyTemplate "templates/blog.html" blogPostsCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
-
+  match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+defaultPostCtx :: Context String
+defaultPostCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
 
-teaserCtx = 
-    teaserField "teaser" "content" `mappend` postCtx
+teaserCtx =
+  teaserField "teaser" "content" `mappend` defaultPostCtx
